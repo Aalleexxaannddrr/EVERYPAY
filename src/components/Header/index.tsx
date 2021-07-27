b99aab80d6e1855f9ecf2588a1d7f04706796966
Aalleexxaannddrr <alexandr.operman@gamil.com>
2021-07-27 17:12:49 +0800

    Navbar is done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,26 @@
-import React, {createRef, RefObject, useEffect, useState} from "react"
+import React, {useState} from "react"
 import classNames from 'classnames'
 
 const NAV_LiNKS = [
-    {name: 'Как это работает?', scrlPts: '10', keyword: 'HowDoesItWork'},
-    {name: 'Для кого', scrlPts: '20', keyword: 'ForWhom'},
-    {name: 'Кейсы', scrlPts: '30', keyword: 'Cases'},
-    {name: 'Тарифы', scrlPts: '40', keyword: 'Tariffs'},
-    {name: 'Безопасность', scrlPts: '50', keyword: 'Secure'}
+    {name: 'Как это работает?', ENname:'How it works?', scrlPts: '10', keyword: 'HowItWorks'},
+    {name: 'Для кого', ENname:'For whom', scrlPts: '20', keyword: 'ForWhom'},
+    {name: 'Кейсы', ENname:'Cases', scrlPts: '30', keyword: 'Cases'},
+    {name: 'Тарифы', ENname: 'Tariffs', scrlPts: '40', keyword: 'Tariffs'},
+    {name: 'Безопасность', ENname:'Safety', scrlPts: '50', keyword: 'Safety'}
 ]
 
 const Header: React.FC = () => {
     const [menuVisible, setMenuVisible] = useState(false)
-    const menuPopupRef: RefObject<HTMLDivElement> = createRef()
-    const menuButtonRef: RefObject<HTMLDivElement> = createRef()
+    const [EN, setEN] = useState(false)
+
+    const toggleEN = (i) => {
+        if (EN !== i) {
+            setEN(i)
+        }
+    }
 
     const toggleMenuVisible = () => {
         setMenuVisible(!menuVisible)
-    }
-
-    const handleClickOutside = (e: any) => {
-        if (!menuVisible) return
-        if (menuButtonRef.current && menuButtonRef.current.contains(e.target)) return
-        if (menuPopupRef.current && !menuPopupRef.current.contains(e.target)) {
-            setMenuVisible(false)
-        }
     }
 
     const handleMenuClick = (scrlPts) => {
@@ -36,45 +33,46 @@ const Header: React.FC = () => {
         })
     }
 
-    useEffect(() => {
-        document.addEventListener('mousedown', handleClickOutside)
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside)
-        }
-    }, [menuPopupRef])
-
     return (
         <div className="header-wrapper">
             <header className="header">
                 <button
-                    className="logo_btn"
+                    className="header__logo"
                     onClick={() => handleMenuClick(0)}
                 >
-                    <img src="/img/logo1.png" alt="" className="header_logo-img" />
+                    <img src="/img/logo1.png" alt="" className="header__logo-img" />
                 </button>
-                <nav className="header_nav">
-                    <ul className="header_list">
+                <nav className="header__nav">
+                    <ul className="null header__list">
                         {NAV_LiNKS.map((item, i) => (
-                            <li key={i} className="header_item">
-                                <button className="menu_btn" onClick={() => handleMenuClick(item.scrlPts)}>
-                                    {item.name}
+                            <li key={i} className="header__item">
+                                <button className="header__link" onClick={() => handleMenuClick(item.scrlPts)}>
+                                    {EN ? item.ENname : item.name}
                                 </button>
                             </li>
                         ))}
                     </ul>
                 </nav>
-                <div className="header_func">
-                    <input type="radio" id="RU" value="RU" name="language" checked />
-                    <label htmlFor="RU">RU</label>
-
-                    <input type="radio" id="EN" value="RU" name="language" />
-                    <label htmlFor="EN">EN</label>
+                <div className="header__language">
+                    <button className={classNames('header__language-btn', {
+                        'header__active-language': !EN
+                    })} onClick={() => toggleEN(false)}>
+                        RU
+                    </button>
+                    <button className={classNames('header__language-btn', {
+                        'header__active-language': EN
+                    })} onClick={() => toggleEN(true)}>
+                        EN
+                    </button>
                 </div>
-                <button className="connect_btn">
-                    Подключиться
-                </button>
+                <div className="header__btn">
+                    <button className="btn btn--hollow">
+                        <span className="btn__label--hollow">
+                            {EN ? 'Connect' : 'Подключиться'}
+                        </span>
+                    </button>
+                </div>
                 <div
-                    ref={menuButtonRef}
                     className={classNames('menu-btn menu-btn--ml', {
                         'active': menuVisible
                     })}
@@ -83,50 +81,26 @@ const Header: React.FC = () => {
                     <span></span>
                 </div>
                 <div
-                    ref={menuPopupRef}
                     className={classNames('header__menu', {
                         'header__menu--visible': menuVisible,
                     })}
                 >
-                    <div
-                        className="header__menu__overlay"
-                        onClick={toggleMenuVisible}
-                    />
                     <div className="header__menu__content-wrapper">
-                        <div className="header__menu__content">
-                            <div className="header__menu__close">
-                                <div
-                                    ref={menuButtonRef}
-                                    className={classNames('menu-btn menu-btn--ml', {
-                                        'active': menuVisible,
-                                    })}
-                                    onClick={toggleMenuVisible}
-                                >
-                                    <span></span>
-                                </div>
-                            </div>
-                            <button
-                                className="logo_btn"
-                                onClick={() => handleMenuClick(0)}
-                            >
-                                <img src="/img/logo1.png" alt="" className="header_logo-img" />
+                        <ul className="null header__menu__nav">
+                            {NAV_LiNKS.map((item, i) => (
+                                <li key={i} className="header__item">
+                                    <button className="header__link" onClick={() => handleMenuClick(item.scrlPts)}>
+                                        {EN ? item.ENname : item.name}
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="header__menu__bottom">
+                            <button className="btn btn--hollow">
+                                <span className="btn__label--hollow">
+                                    {EN ? 'Connect' : 'Подключиться'}
+                                </span>
                             </button>
-                            <ul className="header_list">
-                                {NAV_LiNKS.map((item, i) => (
-                                    <li key={i} className="header_item">
-                                        <button className="menu_btn" onClick={() => handleMenuClick(item.scrlPts)}>
-                                            {item.name}
-                                        </button>
-                                    </li>
-                                ))}
-                            </ul>
-                            <div className="header__menu__bottom">
-                                <input type="radio" id="RU" value="RU" name="language" checked />
-                                <label htmlFor="RU">RU</label>
-
-                                <input type="radio" id="EN" value="RU" name="language" />
-                                <label htmlFor="EN">EN</label>
-                            </div>
                         </div>
                     </div>
                 </div>
