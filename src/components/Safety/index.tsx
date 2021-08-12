17c2ceceeae2067c32503e917a3ff6865b2f9609
Aalleexxaannddrr <alexandr.operman@gamil.com>
2021-08-12 17:52:54 +0800

    Fix Safety

--- src/components/Safety/index.tsx
+++ src/components/Safety/index.tsx
@@ -5,20 +5,24 @@ const Safety: React.FC = () => {
     const language = useContext(LanguageContext)
 
     return (
-        <div className="review-item review-item">
-            <img className="review-item__img2 review-item__img2__big" src="/img/safety/1.png" alt="" />
-            <div className="review-item__left review-item__left__safety">
-                <h3 className="null review-item__titles">
-                    {(language.language === 'EN') ?
-                        "Safety" :
-                        "Безопасность"}
-                </h3>
-                <div className="safety">
-                    <img className="review-item__img2 review-item__img2__hidden" src="/img/safety/1.png" alt="" />
-                    <p className="null review-item__text2">
+        <div className="safety">
+            <h3 className="safety__title">
+                {(language.language === 'EN') ?
+                    "Safety" :
+                    "Безопасность"}
+            </h3>
+            <div className="safety__container">
+                <img className="safety__img" src="/img/safety/1.png" alt="" />
+                <div className="safety__right">
+                    <h3 className="safety__right__title">
+                        {(language.language === 'EN') ?
+                            "Safety" :
+                            "Безопасность"}
+                    </h3>
+                    <p className="safety__right__text">
                         {(language.language === 'EN') ?
                             "Using Everypay is completely safe: connection to a bank account is carried out in the \"Only for receiving statements\" mode, and information storage in the system is protected and depersonalized." :
-                            "Использование Everypay полностью безопасно: подключение к банковскому счету осуществляется в режиме «Только для получения выписок», а хранение информации в системе защищено и деперсонализировано."}
+                            "Использование Everypay полностью безопасно: подключение к банковскому счету осуществляется в режиме «только для получения выписок», а хранение информации в системе защищено и деперсонализировано."}
                     </p>
                 </div>
             </div>
